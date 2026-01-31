e6e860fe667b688b92a844b5ef7e73f05d52fbab
Nathanael-Tekalign <156130272+Nathanael-Tekalign@users.noreply.github.com>
2026-01-31 11:47:41 -0600

    Update TechnicanController.cs

--- SportsPro/Controllers/TechnicanController.cs
+++ SportsPro/Controllers/TechnicanController.cs
@@ -16,61 +16,31 @@ namespace SportsPro.Controllers
         [HttpGet]
 public IActionResult Add()
 {
-    ViewBag.Action = "Add";
-    ViewBag.Categories = new SelectList(context.Categories.OrderBy(c => c.Name), "CategoryId", "Name");
-    return View("Edit", new Contact());
+
 }
 
 [HttpGet]
-public IActionResult Edit(int id)
+public IActionResult Edit()
 {
-    ViewBag.Action = "Edit";
-    var contact = context.Contacts.Find(id);
-    ViewBag.Categories = new SelectList(context.Categories.OrderBy(c => c.Name), "CategoryId", "Name", contact.CategoryId);
-    return View(contact);
+   
 }
 
 [HttpPost]
-public IActionResult Edit(Contact contact)
+public IActionResult Edit()
 {
-    if (ModelState.IsValid)
-    {
-        if (contact.ContactId == 0)
-        {
-            context.Contacts.Add(contact);
-        }
-        else
-        {
-            context.Contacts.Update(contact);
-        }
-
-        context.SaveChanges();
-        return RedirectToAction("Index", "Home");
-    }
-    else
-    {
-        ViewBag.Action = (contact.ContactId == 0) ? "Add" : "Edit";
-        ViewBag.Categories = new SelectList(context.Categories.OrderBy(c => c.Name), "CategoryId", "Name", contact.CategoryId);
-        return View(contact);
-    }
+   
 }
 
 [HttpGet]
-public IActionResult Delete(int id)
+public IActionResult Delete()
 {
-    var contact = context.Contacts
-        .Include(c => c.Category)
-        .FirstOrDefault(c => c.ContactId == id);
-
-    return View(contact);
+    
 }
 
 [HttpPost]
-public IActionResult Delete(Contact contact)
+public IActionResult Delete()
 {
-    context.Contacts.Remove(contact);
-    context.SaveChanges();
-    return RedirectToAction("Index", "Home");
+   
 }
 
     }

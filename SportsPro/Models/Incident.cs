366199bb30d3209ec296abb315fc65c5a15d71dc
OhStreet <jstreet11@live.com>
2026-02-03 14:42:19 -0600

    Update Incident.cs Model

--- SportsPro/Models/Incident.cs
+++ SportsPro/Models/Incident.cs
@@ -1,21 +1,37 @@
-﻿namespace SportsPro.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace SportsPro.Models
 {
     public class Incident
     {
-		public int IncidentID { get; set; }
-		public string Title { get; set; } = string.Empty;
+        public int IncidentID { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be 3–100 characters.")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(2000, MinimumLength = 5, ErrorMessage = "Description must be 5–2000 characters.")]
         public string Description { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
         public DateTime DateOpened { get; set; } = DateTime.Now;
+
+        [DataType(DataType.Date)]
         public DateTime? DateClosed { get; set; } = null;
 
-		public int CustomerID { get; set; }                   // foreign key property
-		public Customer Customer { get; set; } = null!;       // navigation property
+        // FK validation
+        [Range(1, int.MaxValue, ErrorMessage = "Customer is required.")]
+        public int CustomerID { get; set; }                   // foreign key property
+		public Customer? Customer { get; set; } = null!;       // navigation property
 
-		public int ProductID { get; set; }                    // foreign key property
-		public Product Product { get; set; } = null!;         // navigation property
+        [Range(1, int.MaxValue, ErrorMessage = "Product is required.")]
+        public int ProductID { get; set; }                    // foreign key property
+		public Product? Product { get; set; } = null!;         // navigation property
 
-		public int TechnicianID { get; set; }                 // foreign key property 
-		public Technician Technician { get; set; } = null!;   // navigation property
+        [Range(1, int.MaxValue, ErrorMessage = "Technician is required.")]
+        public int TechnicianID { get; set; }                 // foreign key property 
+		public Technician? Technician { get; set; } = null!;   // navigation property
 
 		
 	}

36ecd57a1dd1a593cf4f36d5d5a9e9698778356d
Benjamin Terzic <52902523+CloneMalone@users.noreply.github.com>
2026-02-18 19:57:06 -0600

    Adding ViewModel classes

---
+++ SportsPro/Models/IncidentFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SportsPro.Models
+{
+
+    // ViewModel for Add/Edit views to allow for dropdown lists and cleaner code.
+    public class IncidentFormViewModel
+    {
+        public string OperationMode { get; set; } = "Add";
+        public Incident CurrentIncident { get; set; } = new Incident();
+
+        public List<Customer> Customers { get; set; } = new List<Customer>();
+        public List<Product> Products { get; set; } = new List<Product>();
+        public List<Technician> Technicians { get; set; } = new List<Technician>();
+
+
+    }
+}
---
+++ SportsPro/Models/IncidentListViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SportsPro.Models
+{   
+    // ViewModel for the main list view to allow for future filtering by status, etc. and cleaner code in the view.
+    public class IncidentListViewModel
+    {
+        public string IncidentStatus { get; set; } = "all";
+
+        public List<Incident> Incidents { get; set; } = new List<Incident>();
+
+    }
+}

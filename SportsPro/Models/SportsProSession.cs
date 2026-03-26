97da3f0254dae7e5017efb0b4dc98b5a5f089a75
Benjamin Terzic <52902523+CloneMalone@users.noreply.github.com>
2026-03-25 19:10:06 -0500

    Added SessionExtentions & SportsProSession
    Session Helpers

---
+++ SportsPro/Models/SportsProSession.cs
@@ -0,0 +1,29 @@
+﻿namespace SportsPro.Models
+{
+    public class SportsProSession
+    {
+        private const string TechKey = "technicianid";
+
+        private ISession session { get; set; }
+
+        public SportsProSession(ISession session)
+        {
+            this.session = session;
+        }
+
+        public void SetTechnicianId(int techId)
+        {
+            session.SetInt32(TechKey, techId);
+        }
+
+        public int? GetTechnicianId()
+        {
+            return session.GetInt32(TechKey);
+        }
+
+        public void RemoveTechnician()
+        {
+            session.Remove(TechKey);
+        }
+    }
+}

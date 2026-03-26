359ee97747bfa836639eadf38364c39a520c7cac
Benjamin Terzic <52902523+CloneMalone@users.noreply.github.com>
2026-03-25 19:10:36 -0500

    Add Technician property to IncidentListViewModel
    Added Technician property to IncidentListViewModel.

--- SportsPro/Models/IncidentListViewModel.cs
+++ SportsPro/Models/IncidentListViewModel.cs
@@ -1,9 +1,11 @@
-﻿namespace SportsPro.Models
+namespace SportsPro.Models
 {   
     // ViewModel for the main list view to allow for future filtering by status, etc. and cleaner code in the view.
     public class IncidentListViewModel
     {
         public string IncidentStatus { get; set; } = "all";
+
+        public Technician? Technician { get; set; } = new Technician();
 
         public List<Incident> Incidents { get; set; } = new List<Incident>();
 

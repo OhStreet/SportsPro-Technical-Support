020567aa644e8fd5c3cf5a6bac347ce94f2da7d8
Benjamin Terzic <52902523+CloneMalone@users.noreply.github.com>
2026-04-23 18:56:52 -0500

    Refactor CustomerController to use UnitOfWork pattern

--- SportsPro/Controllers/CustomerController.cs
+++ SportsPro/Controllers/CustomerController.cs
@@ -1,38 +1,57 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SportsPro.DataLayer;
 using SportsPro.Models;
 
 namespace SportsPro.Controllers
 {
     public class CustomerController : Controller
     {
-        private SportsProContext context;
 
-        public CustomerController(SportsProContext context)
+        // Using dependency injection to get an instance of the UnitOfWork class,
+        // which provides access to the repositories for the application!
+        private IUnitOfWork Data { get; set; }
+
+
+
+
+        // The constructor takes an IUnitOfWork parameter
+        // and assigns it to the private Data fieldd.
+        public CustomerController(IUnitOfWork data)
         {
-            this.context = context;
+            this.Data = data;
         }
 
-        // Load countries for dropdown
+
+
+
+        // Uses the UnitOfWork to get a list of countries from the database,
+        // ordered by name, and stores it in the ViewBag for use in the views.
         private void LoadCountries()
         {
-            ViewBag.Countries = context.Countries
-                .OrderBy(c => c.Name)
-                .ToList();
+            ViewBag.Countries = Data.Countries.List(new QueryOptions<Country>
+            {
+                OrderBy = c => c.Name
+            });
         }
 
+
+        // Uses the UnitOfWork to get a list of customers from the database,
+        // including their associated country information, ordered by
+        // last name and then first name, and passes it to the view for display.
         [HttpGet]
         [Route("Customers")]
         public IActionResult List()
         {
-            var customers = context.Customers
-                .Include(c => c.Country)
-                .OrderBy(c => c.LastName)
-                .ThenBy(c => c.FirstName)
-                .ToList();
-
+            var customers = Data.Customers.List(new QueryOptions<Customer>
+            {
+                Includes = "Country",
+                OrderBy = c => c.LastName,
+                ThenOrderBy = c => c.FirstName
+            });
             return View(customers);
         }
+
 
         [HttpGet]
         public IActionResult Add()
@@ -42,33 +61,39 @@ namespace SportsPro.Controllers
             return View("Edit", new Customer());
         }
 
+
+        // Uses UnitOfWork to prepare the view for editing an existing customer.
+        // It retrieves the customer with the specified ID from the database,
+        // including its associated country information, and passes it to the
+        // view for editing.
         [HttpGet]
         public IActionResult Edit(int id)
         {
             ViewBag.Action = "Edit";
             LoadCountries();
-
-            var customer = context.Customers.Find(id);
+            var customer = Data.Customers.Get(id);
             return View("Edit", customer);
         }
 
+
+
+        // Uses UnitOfWork to save changes to a customer.
+        // If the model state is valid,
+        // it inserts a new customer or updates an existing one,
+        // then saves the changes
+        // to the database and redirects to the customer list view.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Edit(Customer customer)
         {
-            System.Diagnostics.Debug.WriteLine($"CountryID POSTED = '{customer.CountryID}'");
             if (ModelState.IsValid)
             {
                 if (customer.CustomerID == 0)
-                {
-                    context.Customers.Add(customer);
-                }
+                    Data.Customers.Insert(customer);
                 else
-                {
-                    context.Customers.Update(customer);
-                }
+                    Data.Customers.Update(customer);
 
-                context.SaveChanges();
+                Data.Save();
                 return RedirectToAction("List", "Customer");
             }
             else
@@ -79,22 +104,29 @@ namespace SportsPro.Controllers
             }
         }
 
+
+        // Uses UnitOfWork to navigate to the delete confirmation view
+        // for a customer.
         [HttpGet]
         public IActionResult Delete(int id)
         {
-            var customer = context.Customers
-                .Include(c => c.Country)
-                .FirstOrDefault(c => c.CustomerID == id);
-
+            var customer = Data.Customers.Get(new QueryOptions<Customer>
+            {
+                Includes = "Country",
+                Where = c => c.CustomerID == id
+            });
             return View(customer);
         }
 
+
+
+        // Uses UnitOfWork to delete a customer.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Delete(Customer customer)
         {
-            context.Customers.Remove(customer);
-            context.SaveChanges();
+            Data.Customers.Delete(customer);
+            Data.Save();
             return RedirectToAction("List", "Customer");
         }
     }

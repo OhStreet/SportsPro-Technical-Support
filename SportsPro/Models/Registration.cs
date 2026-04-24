37d66c0e6e05d17f2a6b430c456c09af563a293c
Benjamin Terzic <52902523+CloneMalone@users.noreply.github.com>
2026-04-23 19:23:52 -0500

    Add Registration Class

---
+++ SportsPro/Models/Registration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace SportsPro.Models
+{
+    public class Registration
+    {
+        public int CustomerID { get; set; }
+        [ValidateNever]
+        public Customer Customer { get; set; } = null!;
+
+        public int ProductID { get; set; }
+        [ValidateNever]
+        public Product Product { get; set; } = null!;
+    }
+}

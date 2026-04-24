cae86971b39d11ac4f174723bfd5cdc4257a3c9c
Benjamin Terzic <52902523+CloneMalone@users.noreply.github.com>
2026-04-23 19:16:39 -0500

    Add files via upload

---
+++ SportsPro/DataLayer/Configuration/RegistrationConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SportsPro.Models;
+
+namespace SportsPro.DataLayer.Configuration
+{
+    internal class RegistrationConfig : IEntityTypeConfiguration<Registration>
+    {
+        public void Configure(EntityTypeBuilder<Registration> entity)
+        {
+            // Composite primary key
+            entity.HasKey(r => new { r.CustomerID, r.ProductID });
+
+            // Many-to-many relationship Customer -> Registrations -> Product
+            entity.HasOne(r => r.Customer)
+                  .WithMany(c => c.Registrations)
+                  .HasForeignKey(r => r.CustomerID);
+
+            entity.HasOne(r => r.Product)
+                  .WithMany(p => p.Registrations)
+                  .HasForeignKey(r => r.ProductID);
+        }
+    }
+}
